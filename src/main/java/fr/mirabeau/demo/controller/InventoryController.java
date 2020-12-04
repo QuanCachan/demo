9ee3f3a58c7deb709751775bf683ffd438bef8e6
QuanCachan <mqtran.cachan@gmail.com>
2020-12-04 16:40:05 +0100

    add SLF4J + LOG4J into project

--- src/main/java/fr/mirabeau/demo/controller/InventoryController.java
+++ src/main/java/fr/mirabeau/demo/controller/InventoryController.java
@@ -2,6 +2,8 @@ package fr.mirabeau.demo.controller;
 
 import fr.mirabeau.demo.entity.Inventory;
 import fr.mirabeau.demo.service.InventoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +17,22 @@ import java.util.List;
 @RestController
 @RequestMapping("inventories")
 public class InventoryController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(InventoryController.class);
+
     @Autowired
     private InventoryService inventoryService;
 
     @GetMapping(value = "/{name}")
     public List<Inventory> getInventoryByName(@PathVariable String name) {
+        if (LOGGER.isDebugEnabled()){
+            LOGGER.debug("get an Inventory by name");
+        }
         return inventoryService.getInventoryByName(name);
     }
 
     @GetMapping()
     public List<Inventory> getAllInventories() {
+        LOGGER.info("get all Inventories");
         return inventoryService.getAllInventories();
     }
 }
